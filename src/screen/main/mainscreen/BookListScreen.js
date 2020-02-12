a648c40479b7f3f3e5b9ba8b8302133daa7c5d49
jungwoole91 <jungwoolee91@gmail.com>
2020-02-13 04:49:28 +0900

    장 선택 화면 구성 완료 / sqlite 적용 완료

--- src/screen/main/mainscreen/BookListScreen.js
+++ src/screen/main/mainscreen/BookListScreen.js
@@ -16,25 +16,25 @@ export default class BookListScreen extends Component {
 
   render() {
     const items = [
-      { name: '창', code: '창세기' }, { name: '출', code: '출애굽기' },
-      { name: '레', code: '레위기' }, { name: '민', code: '민수기' },
-      { name: '신', code: '신명기' }, { name: '수', code: '여호수아' },
-      { name: '삿', code: '사사기' }, { name: '룻', code: '룻기' },
-      { name: '삼상', code: '사무엘상' }, { name: '삼하', code: '사무엘하' },
-      { name: '왕상', code: '열왕기상' }, { name: '왕하', code: '열왕기하' },
-      { name: '대상', code: '역대상' }, { name: '대하', code: '역대하' },
-      { name: '스', code: '에스라' }, { name: '느', code: '느헤미야' },
-      { name: '에', code: '에스더' }, { name: '욥', code: '욥기' },
-      { name: '시', code: '시편' }, { name: '잠', code: '잠언' },
-      { name: '전', code: '전도서' }, { name: '아', code: '아가' },
-      { name: '사', code: '이사야' }, { name: '렘', code: '예레미야' },
-      { name: '애', code: '예레미아애가' }, { name: '겔', code: '에스겔' },
-      { name: '단', code: '다니엘' }, { name: '호', code: '호세아' },
-      { name: '욜', code: '요엘' }, { name: '암', code: '아보스' },
-      { name: '미', code: '미가' }, { name: '나', code: '나훔' },
-      { name: '합', code: '하박국' }, { name: '습', code: '스바냐' },
-      { name: '학', code: '학개' }, { name: '슥', code: '스가랴' },
-      { name: '말', code: '말라기' },
+      { name: '창', bookName: '창세기', bookCode: 1}, { name: '출', bookName: '출애굽기', bookCode: 2},
+      { name: '레', bookName: '레위기', bookCode: 3}, { name: '민', bookName: '민수기' , bookCode: 4},
+      { name: '신', bookName: '신명기', bookCode: 5}, { name: '수', bookName: '여호수아', bookCode: 6 },
+      { name: '삿', bookName: '사사기', bookCode: 7}, { name: '룻', bookName: '룻기', bookCode: 8 },
+      { name: '삼상', bookName: '사무엘상', bookCode: 9}, { name: '삼하', bookName: '사무엘하', bookCode: 10 },
+      { name: '왕상', bookName: '열왕기상', bookCode: 11 }, { name: '왕하', bookName: '열왕기하', bookCode: 12 },
+      { name: '대상', bookName: '역대상', bookCode: 13 }, { name: '대하', bookName: '역대하', bookCode: 14 },
+      { name: '스', bookName: '에스라', bookCode: 15 }, { name: '느', bookName: '느헤미야', bookCode: 16 },
+      { name: '에', bookName: '에스더', bookCode: 17 }, { name: '욥', bookName: '욥기', bookCode: 18 },
+      { name: '시', bookName: '시편', bookCode: 19 }, { name: '잠', bookName: '잠언', bookCode: 20 },
+      { name: '전', bookName: '전도서', bookCode: 21 }, { name: '아', bookName: '아가', bookCode: 22 },
+      { name: '사', bookName: '이사야', bookCode: 23 }, { name: '렘', bookName: '예레미야', bookCode: 24 },
+      { name: '애', bookName: '예레미아애가', bookCode: 25 }, { name: '겔', bookName: '에스겔', bookCode: 26 },
+      { name: '단', bookName: '다니엘', bookCode: 27 }, { name: '호', bookName: '호세아', bookCode: 28 },
+      { name: '욜', bookName: '요엘', bookCode: 29 }, { name: '암', bookName: '아보스', bookCode: 30 },
+      { name: '미', bookName: '미가', bookCode: 31 }, { name: '나', bookName: '나훔', bookCode: 32 },
+      { name: '합', bookName: '하박국', bookCode: 33 }, { name: '습', bookName: '스바냐', bookCode: 34 },
+      { name: '학', bookName: '학개', bookCode: 35 }, { name: '슥', bookName: '스가랴', bookCode: 36 },
+      { name: '말', bookName: '말라기', bookCode: 37 },
     ];
 
     return (
@@ -50,10 +50,10 @@ export default class BookListScreen extends Component {
             <TouchableOpacity
               style={[styles.itemContainer, { backgroundColor: '#F3F4F9' }]}
               // sqlite조회를 위한 bookId를 넘겨줍니다.
-              onPress={() => this.props.navigation.navigate('ChapterListScreen',{bookName: `${item.name}`, bookIndex: index + 1})}
+              onPress={() => this.props.navigation.navigate('ChapterListScreen',{bookName: item.bookName, bookIndex: index + 1, bookCode: item.bookCode})}
             >
               <Text style={styles.itemName}>{item.name}</Text>
-              <Text style={styles.itemCode}>{item.code}</Text>
+              <Text style={styles.itemCode}>{item.bookName}</Text>
             </TouchableOpacity>
           )}
         />
--- src/screen/main/mainscreen/ChapterListScreen.js
+++ src/screen/main/mainscreen/ChapterListScreen.js
@@ -1,34 +1,111 @@
 import React, { Component } from 'react';
 import {
-
   StyleSheet,
   View,
   Text,
-  Image,
-  Alert,
-  Button,
-  TouchableOpacity
+  TouchableOpacity,
+  FlatList
 
 } from 'react-native';
+
+import SQLite from 'react-native-sqlite-storage';
+
+// SQLITE 성공/실패 예외처리
+const errorCallback = (e) => {
+  console.log('Error');
+  console.log(e.message);
+};
+const okCallback = (result) => {
+  console.log('okay');
+  console.log(result);
+};
 
 export default class ChapterListScreen extends Component {
   state = {
     bookName: '업데이트전',
-    navigation: null
+    bookCode: 0,
+    navigation: null,
+    chapterItems: [],
   };
 
   componentDidMount() {
     const { route } = this.props;
-    this.setState({bookName: route.params.bookName});
-    this.setState({navigation: this.props.navigation});
+    this.setState({
+      bookName: route.params.bookName,
+      navigation: this.props.navigation,
+      bookCode: route.params.bookCode
+    });
 
+    /**
+     * SQLITE LOAD
+     */
+    let bibleDB = SQLite.openDatabase({name : "bible.db", createFromLocation : 1}, okCallback, errorCallback);
+    bibleDB.transaction((tx) => {
+      console.log(`bookCode : ${this.state.bookCode}`);
+      tx.executeSql(`SELECT max(chapter) as count FROM bible_korHRV where book = ${this.state.bookCode}`, [], (tx, results) => {
+        let chapterItemsLength = results.rows.item(0).count;
+        const chapterItems = [];
+        for (let i = 0; i < chapterItemsLength ; i++) {
+          chapterItems.push({content: this.state.bookName})
+        }
+        this.setState({chapterItems});
+      })
+    })
   }
 
   render() {
     return (
-      <View>
-        <Text>{this.state.bookName}</Text>
+      <View style={styles.container}>
+        <Text style={styles.titleText}>장을 선택해주세요.</Text>
+        <Text>{this.state.result}</Text>
+        <FlatList
+          style={styles.flatList}
+          data={this.state.chapterItems}
+          keyExtractor={item => item.id}
+          renderItem={({item, index}) => {
+            return (
+                <TouchableOpacity style={styles.flatListItem}>
+                  <Text style={styles.flatListItemText}>{index + 1}.    {item.content}{index + 1}장</Text>
+                </TouchableOpacity>
+              )
+          }}
+        />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 20,
+    paddingRight: 20,
+    backgroundColor: 'white'
+  },
+  titleText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5
+  },
+  flatList: {
+
+  },
+  flatListItem: {
+    height: 57,
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'column',
+    paddingTop: 30,
+    paddingBottom: 30,
+    paddingLeft: 2,
+    paddingRight: 2,
+    borderBottomColor: '#AABBCC',
+    borderBottomWidth: 1,
+  },
+  flatListItemText: {
+    color: 'black'
+  }
+});
